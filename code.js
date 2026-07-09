4ff847a6e1650addd7ae05ac64a85337bc0d3c32
Abubazard <casualdude180@gmail.com>
2026-07-09 13:43:05 +0100

    demn. welp. scrapped the idea of multiple plygon types. will use i'll only a square in the game

--- code.js
+++ code.js
@@ -3,26 +3,57 @@ const ctx = canvas.getContext("2d")
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
+const colorSet={
+    rotatingBlock:['grey']
+}
 
-function drawPolygon(n,posx,posy,rad,deg){
+
+function drawPlayer(phase){
+    ctx.fillStyle='grey'
+    ctx.fillRect(canvas.width/2-100,canvas.height/2-100,200,200)
+}
+
+function drawBlock(phase,x,y,width,height,color){
+    dwidth=width/2
+    bWidth=y+10
+    tWidth=y-10
     ctx.beginPath()
-    for(let i=0; i<n; i++){
-        const angle = ((Math.PI/3)*i-Math.PI/2)+deg
-        const x = posx+rad*Math.cos(angle)
-        const y = posy+rad*Math.sin(angle)
-
-        if(i===0){
-            ctx.moveTo(x,y)
-        } else {
-            ctx.lineTo(x,y)
-        }
-    }
+    ctx.moveTo(x-(dwidth+height), y+(height/2))
+    ctx.lineTo(x+dwidth+height, y+(height/2))
+    ctx.lineTo(x+dwidth, y-height)
+    ctx.lineTo(x-dwidth, y-height)
     ctx.closePath()
-    ctx.stroke()
+    ctx.fillStyle=color
+    ctx.fill()
+}
+
+
+
+rotation = 90
+
+function update(delta){
+    //rotation+=delta*1
 }
 
 function render(){
     ctx.clearRect(0,0,canvas.width,canvas.height)
+    drawPlayer(canvas.width/2,canvas.height/2,200,rotation)
+    drawBlock(0,300,300,200,20,'red')
 }
 
-drawPolygon(6,canvas.width/2,canvas.height/2,100,0)
+
+let lastTime = 0
+const FPS = 60
+const timestep = 1000/FPS
+function gameLoop(ctime){
+    const deltaTime = (ctime - lastTime)/1000
+
+    if (ctime - lastTime >= timestep){
+        update(deltaTime)
+        render()
+        lastTime = ctime
+    }
+
+    requestAnimationFrame(gameLoop)
+}
+requestAnimationFrame(gameLoop)
